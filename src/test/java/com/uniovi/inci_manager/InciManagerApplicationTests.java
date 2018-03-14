6ecc5982a04374e9b076cd4ff132d6c38967f15f
Miguel Martínez Serrano <miguelms.ovi@gmail.com>
2018-03-14 19:47:18 +0100

    prueba funcionamiento añado test con código para comprobar que codecov se ejecuta

--- src/test/java/com/uniovi/inci_manager/InciManagerApplicationTests.java
+++ src/test/java/com/uniovi/inci_manager/InciManagerApplicationTests.java
@@ -1,5 +1,6 @@
 package com.uniovi.inci_manager;
 
+import com.uniovi.inci_manager.db_management.model.Incidence;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,9 +10,16 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class InciManagerApplicationTests {
 
-	@Test
-	public void contextLoads() {
-		// TODO: testear
-	}
+    @Test
+    public void contextLoads() {
+        // TODO: testear
+    }
+
+    @Test
+    public void test1Prueba() {
+        Incidence incidence = new Incidence("agente1", "incidencia que flipas",
+                "bua, ocurrió algo desastroso", "45.6783, 53.6789", null);
+
+    }
 
 }
