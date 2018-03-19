b69c3de97976994faa5592ba994cce9bbb040516
Miguel Martínez Serrano <miguelms.ovi@gmail.com>
2018-03-19 01:15:56 +0100

    test básico de creacion de incidencia para subir el codecov

--- src/test/java/asw/inci_manager/InciManagerApplicationTests.java
+++ src/test/java/asw/inci_manager/InciManagerApplicationTests.java
@@ -1,5 +1,6 @@
 package asw.inci_manager;
 
+import asw.inci_manager.db_management.model.Incidence;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,8 +16,16 @@ public class InciManagerApplicationTests {
     }
 
     @Test
-    public void test1Prueba() {
+    public void testIncidenciaModel() {
+        Incidence i = new Incidence("agente1", "incidencia 1", "descripción de la incidencia", "45.678, 12.896", null);
 
+        assert i.getUsername().equals("agente1");
+        assert i.getIncidenceName().equals("incidencia 1");
+        assert i.getDescription().equals("descripción de la incidencia");
+        assert i.getLocation().equals("45.678, 12.896");
+        assert i.getLabels() == null;
+        assert i.getCampos() == null;
+        assert i.getExpiration() == null;
     }
 
 }
