4e049c83b0108056f4da5de2e696d96179c94394
Kilian Pérez González <kilianpg@gmail.com>
2018-03-21 18:21:25 +0100

    Implementación inicial del servicio de envio de incidencias a través de Kafka.

--- src/main/java/asw/inci_manager/inci_manager_gest/services/IncidenceService.java
+++ src/main/java/asw/inci_manager/inci_manager_gest/services/IncidenceService.java
@@ -1,8 +1,20 @@
 package asw.inci_manager.inci_manager_gest.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import asw.inci_manager.inci_manager_gest.entities.Incidence;
+import asw.inci_manager.kafka_manager.producers.KafkaProducer;
+
 
 @Service
 public class IncidenceService {
 
+	@Autowired
+    KafkaProducer kafkaProducer;
+
+	public void send(Incidence incidence)
+	{
+		kafkaProducer.send("topic", incidence.toString());
+	}
 }
