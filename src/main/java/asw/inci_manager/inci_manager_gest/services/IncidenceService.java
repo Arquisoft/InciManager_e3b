3fd39e64b17b647f3b900257600ce619fad60b05
Kilian Pérez González <kilianpg@gmail.com>
2018-03-21 18:39:49 +0100

    Implementación de la serialización a JSON de las incidencias enviadas a través de Apache Kafka.

--- src/main/java/asw/inci_manager/inci_manager_gest/services/IncidenceService.java
+++ src/main/java/asw/inci_manager/inci_manager_gest/services/IncidenceService.java
@@ -3,6 +3,8 @@ package asw.inci_manager.inci_manager_gest.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.google.gson.Gson;
+
 import asw.inci_manager.inci_manager_gest.entities.Incidence;
 import asw.inci_manager.kafka_manager.producers.KafkaProducer;
 
@@ -12,9 +14,9 @@ public class IncidenceService {
 
 	@Autowired
     KafkaProducer kafkaProducer;
-
+	
 	public void send(Incidence incidence)
 	{
-		kafkaProducer.send("topic", incidence.toString());
+		kafkaProducer.send("topic", new Gson().toJson(incidence));
 	}
 }
