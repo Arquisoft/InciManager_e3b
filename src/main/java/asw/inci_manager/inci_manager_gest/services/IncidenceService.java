c08bc1045a38501a1e05dbdd8bcea26fd0b345ba
Kilian Pérez González <kilianpg@gmail.com>
2018-03-21 18:55:47 +0100

    Añadido mensaje de log en la ejecución del servicio de envio de incidencias.

--- src/main/java/asw/inci_manager/inci_manager_gest/services/IncidenceService.java
+++ src/main/java/asw/inci_manager/inci_manager_gest/services/IncidenceService.java
@@ -1,5 +1,6 @@
 package asw.inci_manager.inci_manager_gest.services;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,11 +13,15 @@ import asw.inci_manager.kafka_manager.producers.KafkaProducer;
 @Service
 public class IncidenceService {
 
+	private static final Logger logger = Logger.getLogger(IncidenceService.class);
+	
 	@Autowired
     KafkaProducer kafkaProducer;
 	
 	public void send(Incidence incidence)
 	{
+		// ToDO: Incorporar un campo topic dinámico o incluirlo como propertie:
 		kafkaProducer.send("topic", new Gson().toJson(incidence));
+		logger.info("Sending incidence \"" + incidence.getIncidenceName() + "\" to topic '" + "topic" +"'");
 	}
 }
