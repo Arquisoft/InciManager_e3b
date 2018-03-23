0fe32a851a2f01133b4b80d75e0803f15a482d26
Miguel Martínez Serrano <miguelms.ovi@gmail.com>
2018-03-23 01:08:51 +0100

    Testeo modelo Agent y modelo Incidencia. Codecov

--- src/test/java/asw/inci_manager/InciManagerApplicationTests.java
+++ src/test/java/asw/inci_manager/InciManagerApplicationTests.java
@@ -13,21 +13,49 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class InciManagerApplicationTests {
 
     @Test
-    public void contextLoads() {
-        // TODO: testear
+    public void testAgentModel() {
+        Agent paco = new Agent("Paco", "123456", "", "paco@gmail.com", "paco", "Person");
+        Agent pepe = new Agent("pepe", "213456", "", "pepe@gmail.com", "pepe", "Person");
+
+        assert paco.equals(paco);
+        assert !paco.equals(null);
+        assert !paco.equals(new Integer(1));
+        assert paco.hashCode() == paco.hashCode();
+
+        pepe.setPassword("pass");
+        pepe.setEmail("pepe2@gmail.com");
+        assert pepe.getEmail().equals("pepe2@gmail.com");
+
+        // Test agente
+        assert paco.getNombre().equals("Paco");
+        assert paco.getPassword().equals("123456");
+        assert paco.getLocation().equals("");
+        assert paco.getIdent().equals("paco");
+        assert paco.getKind().equals("Person");
+        assert paco.getKindCode() == -1; // TODO: cambiar cuando esté implementada,la llamada REST
     }
 
     @Test
     public void testIncidenciaModel() {
-        Agent paco =new Agent("Paco","123456","","paco@gmail.com","paco","Person");
+        Agent paco = new Agent("Paco", "123456", "", "paco@gmail.com", "paco", "Person");
 
         Incidence i = new Incidence(paco, "incidencia 1", "descripción de la incidencia", "45.678, 12.896", "");
 
+        i.getAgent().setPassword("pass");
+        i.getAgent().setEmail("paco@gmail.com");
+
+        // Test agente
         assert i.getAgent().getNombre().equals("Paco");
+        assert i.getAgent().getPassword().equals("pass");
+        assert i.getAgent().getLocation().equals("");
+        assert i.getAgent().getIdent().equals("paco");
+        assert i.getAgent().getKind().equals("Person");
+
+        // Test campos incidencia
         assert i.getIncidenceName().equals("incidencia 1");
         assert i.getDescription().equals("descripción de la incidencia");
         assert i.getLocation().equals("45.678, 12.896");
-        assert i.getLabels() == "";
+        assert i.getLabels().equals("");
         assert i.getCampos() == null;
         assert i.getExpiration() == null;
     }
