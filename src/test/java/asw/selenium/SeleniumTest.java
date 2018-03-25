40d26222d2b7366aff1b5bb32d2c7b017667af85
Daniel Martinez Valeriano <32536834+Gemeto@users.noreply.github.com>
2018-03-25 18:35:42 +0200

    Añadidos test para peticiones REST

--- src/test/java/asw/selenium/SeleniumTest.java
+++ src/test/java/asw/selenium/SeleniumTest.java
@@ -39,9 +39,9 @@ public class SeleniumTest {
 	 *            contraseña
 	 */
 	private void login(String username, String password) {
-		esperaHastaQueCargue("username");
-		rellenar("username", username);
-		rellenar("password", password);
+		esperaHastaQueCargueByName("username");
+		rellenarByName("username", username);
+		rellenarByName("password", password);
 		click("loginButton");
 	}
 
@@ -64,6 +64,29 @@ public class SeleniumTest {
 	 */
 	private void click(String string) {
 		driver.findElement(By.id(string)).click();
+	}
+
+	/**
+	 * Método auxiliar para rellenar campos de formularios
+	 * 
+	 * @param campoID
+	 *            nombre del campo a rellenar
+	 * @param texto
+	 *            valor
+	 */
+	private void rellenarByName(String nombre, String texto) {
+		driver.findElement(By.name(nombre)).sendKeys(texto);
+	}
+	
+	/**
+	 * Método auxiliar para esperar que carguen las páginas. Usa la variable timeout
+	 * de 9 segundos en caso de tardar mucho
+	 * 
+	 * @param string
+	 *            nombre del elemento que esperas que cargue
+	 */
+	private WebElement esperaHastaQueCargueByName(String string) {
+		return (new WebDriverWait(driver, timeout)).until(ExpectedConditions.visibilityOfElementLocated(By.name(string)));
 	}
 
 	/**
