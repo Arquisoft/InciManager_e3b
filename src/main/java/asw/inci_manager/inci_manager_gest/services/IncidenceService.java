103f2ad85c0aec580e13dff4ba1784564680521e
Miguel Martínez Serrano <miguelms.ovi@gmail.com>
2018-03-25 21:14:31 +0200

    Formato UTF sin BOM.

--- src/main/java/asw/inci_manager/inci_manager_gest/services/IncidenceService.java
+++ src/main/java/asw/inci_manager/inci_manager_gest/services/IncidenceService.java
@@ -1,4 +1,4 @@
-﻿package asw.inci_manager.inci_manager_gest.services;
+package asw.inci_manager.inci_manager_gest.services;
 import asw.inci_manager.inci_manager_gest.entities.Agent;
 import asw.inci_manager.inci_manager_gest.entities.Incidence;
 import asw.inci_manager.inci_manager_gest.repositories.IncidenceRepository;
