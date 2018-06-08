6ea574d89f396d169574b02c582735be28769644
UO244730 <UO244730@uniovi.es>
2018-06-08 17:08:33 +0200

    Base de datos en memoria y mockups de kafka

---
+++ src/test/java/kafka/ZooKeeperLocal.java
@@ -0,0 +1,40 @@
+package kafka;
+
+import org.apache.zookeeper.server.ServerConfig;
+import org.apache.zookeeper.server.ZooKeeperServerMain;
+import org.apache.zookeeper.server.quorum.QuorumPeerConfig;
+import scala.util.control.Exception;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ZooKeeperLocal {
+
+    ZooKeeperServerMain zooKeeperServer;
+
+    public ZooKeeperLocal(Properties zkProperties) throws FileNotFoundException, IOException {
+        QuorumPeerConfig quorumConfiguration = new QuorumPeerConfig();
+        try {
+            quorumConfiguration.parseProperties(zkProperties);
+        } catch(Throwable throwable) {
+            throw new RuntimeException(throwable);
+        }
+
+        zooKeeperServer = new ZooKeeperServerMain();
+        final ServerConfig configuration = new ServerConfig();
+        configuration.readFrom(quorumConfiguration);
+
+
+        new Thread() {
+            public void run() {
+                try {
+                    zooKeeperServer.runFromConfig(configuration);
+                } catch (IOException e) {
+                    System.out.println("ZooKeeper Failed");
+                    e.printStackTrace(System.err);
+                }
+            }
+        }.start();
+    }
+}
